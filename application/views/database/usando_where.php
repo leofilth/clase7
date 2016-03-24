159a532979ab6447dfde4214df978e13acdb4d3b
leofilth <leofilth@hotmail.com>
2016-03-23 23:01:04 -0300

    Clase 7 de CodeIgniter, terminada

---
+++ application/views/database/usando_where.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <h1>Ejemplo de DataBase</h1>
+    <h2>Usando where</h2>
+    <ul>
+        <?php
+            echo "<li>"."Nombre: ".$datos->nombre."</li>";
+            echo "<li>"."Correo: ".$datos->correo."</li>";
+            echo "<li>"."Telefono: ".$datos->tel."</li>";
+            echo "<li>"."Fecha: ".$datos->fecha."</li>";
+            echo "<br>";
+        ?>
+    </ul>
+    <p>Ñandú</p>
+</div>
